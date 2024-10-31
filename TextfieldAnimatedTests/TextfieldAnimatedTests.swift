8dad7a1c03de1829efdff526ae1c5845ff83d47f
saadetsimsek <saadetsimsek13@gmail.com>
2024-10-31 14:52:19 +0300

    Initial Commit

---
+++ TextfieldAnimatedTests/TextfieldAnimatedTests.swift
@@ -0,0 +1,17 @@
+//
+//  TextfieldAnimatedTests.swift
+//  TextfieldAnimatedTests
+//
+//  Created by Saadet Şimşek on 31/10/2024.
+//
+
+import Testing
+@testable import TextfieldAnimated
+
+struct TextfieldAnimatedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
