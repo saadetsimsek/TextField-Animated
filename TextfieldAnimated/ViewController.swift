3c7d47e1f04c71d2d78ecf208ec165037915b0ea
saadetsimsek <saadetsimsek13@gmail.com>
2024-11-04 23:34:38 +0300

    animated

--- TextfieldAnimated/ViewController.swift
+++ TextfieldAnimated/ViewController.swift
@@ -24,7 +24,7 @@ class ViewController: UIViewController {
         label.text = " Placeholder "
         label.textColor = .darkGray
         label.font = UIFont(name: "Arial", size: 20)
-        
+       
         return label
     }()
     
@@ -50,13 +50,48 @@ class ViewController: UIViewController {
                                                       y: 0,
                                                       width: 10,
                                                       height: 0))
+        titleLabel.frame.size = titleLabel.getTextSize()
+        titleLabel.anchorPoint = CGPoint(x: 0, y: 0)
         titleLabel.frame.origin = CGPoint(x: mainTextField.frame.origin.x + 10,
                                           y: mainTextField.frame.minY - titleLabel.frame.size.height / 2)
+        
     }
 
 
 }
-
+//MARK: - TextField Delegate
 extension ViewController: UITextFieldDelegate {
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        UIView.animate(withDuration: 0.1) {
+            self.titleLabel.transform = CGAffineTransform(scaleX: 0.6, y: 0.6)
+            self.titleLabel.frame.origin = CGPoint(x: self.mainTextField.frame.origin.x + 15,
+                                                   y: self.mainTextField.frame.minY - self.titleLabel.frame.height / 2)
+        }
+    }
     
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        UIView.animate(withDuration: 0.1) {
+            self.titleLabel.transform = .identity
+            self.titleLabel.frame.origin = CGPoint(x: self.mainTextField.frame.origin.x + 10,
+                                                   y: self.mainTextField.frame.minY - self.titleLabel.frame.size.height / 2)
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
+
+extension UILabel {
+    func getTextSize() -> CGSize {
+        guard let text = self.text else { return CGSize()}
+        let fontName = font.fontName
+        let fontSize = font.pointSize
+        let font = UIFont(name: fontName, size: fontSize)
+        let fontAttributes = [NSAttributedString.Key.font: font]
+        let size = text.size(withAttributes: fontAttributes as [NSAttributedString.Key : Any])
+        return CGSize(width: size.width.rounded() ,
+                      height: size.height.rounded())
+    }
 }
